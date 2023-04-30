acc78806036f222c5183ebf1493ed670716d808d
Sertaç Kocagil <srtc.kcgl@gmail.com>
2023-04-30 14:29:40 +0300

    wip

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,2 @@
 // `Resource` modeli buraya
+const db = require('../../data/db-config')
---
+++ data/migrations/20230430110635_projects_table.js
@@ -0,0 +1,47 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  const all = knex.schema
+    .createTable('projects', (table) => {
+      table.increments('project_id')
+      table.string('project_name').notNullable()
+      table.string('project_description')
+      table.integer('project_completed').notNullable().unsigned().defaultTo(0)
+    })
+    .createTable('resources', (table) => {
+      table.increments('resource_id')
+      table.string('resource_name').notNullable().unique()
+      table.string('resource_description')
+    })
+    .createTable('project_resources', (table) => {
+      table.increments('project_resources_id')
+      table.integer('project_id').references('project_id').inTable('projects')
+      table
+        .integer('resource_id')
+        .references('resource_id')
+        .inTable('resources')
+    })
+    .createTable('tasks', (table) => {
+      table.increments('task_id')
+      table.string('task_description').notNullable()
+      table.string('task_notes')
+      table
+        .integer('project_id')
+        .references('project_id')
+        .inTable('project_resources')
+    })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists('tasks')
+    .dropTableIfExists('project_resources')
+    .dropTableIfExists('resources')
+    .dropTableIfExists('projects')
+}
