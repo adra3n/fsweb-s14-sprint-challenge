78e30c1ec934ed3226636a00ca8720b65d31dd48
Sertaç Kocagil <srtc.kcgl@gmail.com>
2023-05-04 18:46:44 +0300

    updated

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,2 +1,19 @@
 // `Resource` modeli buraya
 const db = require('../../data/db-config')
+
+async function getAll() {
+  return await db('resources')
+}
+
+async function create(resource) {
+  const [resource_id] = await db('resources').insert(resource)
+  const createdResource = await db('resources')
+    .where('resource_id', resource_id)
+    .first()
+  return createdResource
+}
+
+module.exports = {
+  getAll,
+  create,
+}
--- api/resource/router.js
+++ api/resource/router.js
@@ -2,3 +2,23 @@
 const express = require('express')
 const ResourcesModel = require('./model.js')
 const router = express.Router()
+
+router.get('/', async (req, res, next) => {
+  try {
+    const resources = await ResourcesModel.getAll()
+    res.status(200).json(resources)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const createdProject = await ResourcesModel.create(req.body)
+    res.status(201).json(createdProject)
+  } catch (error) {
+    next(error)
+  }
+})
+
+module.exports = router
