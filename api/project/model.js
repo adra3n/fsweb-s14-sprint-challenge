c704ed1ff9030ef685da9e464f9beca1b3e3f4ae
Sertaç Kocagil <srtc.kcgl@gmail.com>
2023-05-04 17:30:24 +0300

    wip

--- api/project/model.js
+++ api/project/model.js
@@ -1,2 +1,25 @@
 // `Proje` modeli buraya
 const db = require('../../data/db-config')
+
+async function getAll() {
+  const projects = db('projects')
+  let transformingProjects = allProjects.map((item) => {
+    return { ...item, project_completed: item.project_completed == 1 }
+  })
+  return transformingProjects
+}
+async function create(project) {
+  const [project_id] = await db('projects').insert(project)
+  const createdProject = await db('projects')
+    .where('project_id', project_id)
+    .first()
+  return {
+    ...createdProject,
+    project_completed: createdProject.project_completed == 1,
+  }
+}
+
+module.export = {
+  getAll,
+  create,
+}
